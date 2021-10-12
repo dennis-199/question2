551672e03e0518dbb5905ba137bc944886b8f683
dennis-199 <otieno.dennis@strathmore.edu>
2021-10-12 18:58:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#The variable sentence stores a string. Write code to determine how many words in sentence start
+# and end with the same letter, including one-letter words. Store the result in the variable same_letter_count.
+sentence = "students flock to the arb for a variety of outdoor activities such as jogging and picnicking"
+
+# Write your code here.
+same_letter_count = sum(w[0] == w[-1] for w in sentence.split())
+print(same_letter_count)
